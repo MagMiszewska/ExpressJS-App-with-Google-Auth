3979fdba7a3cd7540dc4316e90116efaeba1d819
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-06-27 18:05:33 +0200

    Add Google Auth scripts

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '788047566095-ca76e90tqliu05kj4eb371kdh69h7rn3.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '3zXMrjdQ-VQxDxLoHXhvUc5F',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
